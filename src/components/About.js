a4e180691ccd7cb77dd1c2ea60281a73955d5390
Cristian Martinez <2cristianmartinez@gmail.com>
2019-11-17 15:34:19 -0500

    feat: bug fix

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default function About() {
+export default function About(props) {
     return (
         <div>
             <h1>
                 About
+                {
+                    props.data
+                        ? <h2>yes data</h2>
+                        : <h2>no data</h2>
+                }
             </h1>
         </div>
     );
